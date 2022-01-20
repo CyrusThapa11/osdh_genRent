87f045c3e4fe9482c3ea03a7818283cea50315cb
copyNinja1219 <thapacyrus30@gmail.com>
2022-01-20 21:16:52 +0530

    first  commit

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+const userRoutes = require("./routes/userRoutes");
+const renterRoutes = require("./routes/renterRoutes");
+
+app.use("/api/user");
+app.use("/api/renter");
+
 app.use(PORT, () => {
   console.log(`running at : http://localhost:${PORT}/`);
 });
+
+module.exports = { app };
