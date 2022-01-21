92c571da4f82058791de61fe85bf754bd5ff16b5
copyNinja1219 <thapacyrus30@gmail.com>
2022-01-21 18:55:31 +0530

    connect front to back

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const app = express();
+const path = require("path");
 const db = require("./db");
 const userRoutes = require("./routes/userRoutes");
 const renterRoutes = require("./routes/renterRoutes");
@@ -9,6 +10,9 @@ const { urlencoded } = require("express");
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use("/api/user", userRoutes);
 app.use("/api/renter", renterRoutes);
--- backEnd/routes/productRoutes.js
+++ backEnd/routes/productRoutes.js
@@ -2,10 +2,16 @@ const Product = require("../models/product");
 
 const router = require("express").Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    message: "showing all products !",
-  });
+router.get("/", async (req, res) => {
+  const allProducts = await Product.find();
+  console.log("allProducts", allProducts);
+
+  // res.status(200).json({
+  //   message: "showing all products !",
+  //   allProducts,
+  // });
+
+  res.render("products/index", { allProducts });
 });
 
 router.post("/", async (req, res) => {
@@ -14,9 +20,37 @@ router.post("/", async (req, res) => {
   const product = await Product.create(req.body);
   //   const savedP = await product.save();
   console.log("product", product);
-  res.json({
+  res.status(200).json({
     message: "added a product !",
     product,
+  });
+});
+
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log("id is", id);
+  const produ = req.body;
+  console.log("produ", produ);
+  const product = await Product.findByIdAndUpdate(id, req.body, { new: true });
+  //   const savedP = await product.save();
+  console.log("product", product);
+  res.status(200).json({
+    message: "updated a product !",
+    product,
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log("id is", id);
+  const produ = req.body;
+  console.log("produ", produ);
+  const product = await Product.findByIdAndDelete(id);
+  //   const savedP = await product.save();
+  console.log("product", product);
+
+  res.status(200).json({
+    message: "deleted a product !",
   });
 });
 
